b071bc3fb8038f1f13ce8395d2e86ba01b15bab3
appservercodeshare <appservercodeshare@gmail.com>
2016-12-14 02:14:49 +0530

    Update Product.java

--- src/main/java/com/shopeasy/controllers/beans/Product.java
+++ src/main/java/com/shopeasy/controllers/beans/Product.java
@@ -1,26 +1,16 @@
 package com.shopeasy.controllers.beans;
 
-import java.util.Arrays;
-
 public class Product {
 
-	private String productBrand;
 	private String productName;
 	private Double price;
 	private Double mrp;
-	private String measureMentUnit;
-	private Integer quantity;
+	private Integer maxUnit;
+	private Integer minUnit;
+	private String measurementInUnit;
 	private Boolean hasType;
-	private String images;
+	private String image;
 	private SubCategory subCategory;
-
-	public String getProductBrand() {
-		return productBrand;
-	}
-
-	public void setProductBrand(String productBrand) {
-		this.productBrand = productBrand;
-	}
 
 	public String getProductName() {
 		return productName;
@@ -46,20 +36,28 @@ public class Product {
 		this.mrp = mrp;
 	}
 
-	public String getMeasureMentUnit() {
-		return measureMentUnit;
+	public Integer getMaxUnit() {
+		return maxUnit;
 	}
 
-	public void setMeasureMentUnit(String measureMentUnit) {
-		this.measureMentUnit = measureMentUnit;
+	public void setMaxUnit(Integer maxUnit) {
+		this.maxUnit = maxUnit;
 	}
 
-	public Integer getQuantity() {
-		return quantity;
+	public Integer getMinUnit() {
+		return minUnit;
 	}
 
-	public void setQuantity(Integer quantity) {
-		this.quantity = quantity;
+	public void setMinUnit(Integer minUnit) {
+		this.minUnit = minUnit;
+	}
+
+	public String getMeasurementInUnit() {
+		return measurementInUnit;
+	}
+
+	public void setMeasurementInUnit(String measurementInUnit) {
+		this.measurementInUnit = measurementInUnit;
 	}
 
 	public Boolean getHasType() {
@@ -70,12 +68,12 @@ public class Product {
 		this.hasType = hasType;
 	}
 
-	public String getImages() {
-		return images;
+	public String getImage() {
+		return image;
 	}
 
-	public void setImages(String images) {
-		this.images = images;
+	public void setImage(String image) {
+		this.image = image;
 	}
 
 	public SubCategory getSubCategory() {
@@ -88,40 +86,11 @@ public class Product {
 
 	@Override
 	public String toString() {
-		return "Product [productBrand=" + productBrand + ", productName=" + productName + ", price=" + price + ", mrp="
-				+ mrp + ", measureMentUnit=" + measureMentUnit + ", quantity=" + quantity + ", hasType=" + hasType
-				+ ", images=" + images + ", subCategory=" + subCategory + "]";
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((productBrand == null) ? 0 : productBrand.hashCode());
-		result = prime * result + ((productName == null) ? 0 : productName.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Product other = (Product) obj;
-		if (productBrand == null) {
-			if (other.productBrand != null)
-				return false;
-		} else if (!productBrand.equals(other.productBrand))
-			return false;
-		if (productName == null) {
-			if (other.productName != null)
-				return false;
-		} else if (!productName.equals(other.productName))
-			return false;
-		return true;
+		return "Product [productName=" + productName + ", price=" + price
+				+ ", mrp=" + mrp + ", maxUnit=" + maxUnit + ", minUnit="
+				+ minUnit + ", measurementInUnit=" + measurementInUnit
+				+ ", hasType=" + hasType + ", image=" + image
+				+ ", subCategory=" + subCategory + "]";
 	}
 
 }
