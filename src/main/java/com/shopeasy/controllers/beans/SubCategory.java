a956f1343bbfe8151d67016137074f513e7ca9e7
appservercodeshare <appservercodeshare@gmail.com>
2016-12-14 02:17:32 +0530

    Update SubCategory.java

--- src/main/java/com/shopeasy/controllers/beans/SubCategory.java
+++ src/main/java/com/shopeasy/controllers/beans/SubCategory.java
@@ -1,5 +1,6 @@
 package com.shopeasy.controllers.beans;
 
+import java.util.LinkedHashSet;
 import java.util.Set;
 
 public class SubCategory {
@@ -7,6 +8,12 @@ public class SubCategory {
 	private String subCategoryName;
 	private Category category;
 	private Set<Product> productSet;
+
+	
+	
+	public SubCategory() {
+		productSet = new LinkedHashSet<Product>();
+	}
 
 	public String getSubCategoryName() {
 		return subCategoryName;
@@ -32,35 +39,10 @@ public class SubCategory {
 		this.productSet = productSet;
 	}
 
-	@Override
+	/*@Override
 	public String toString() {
-		return "SubCategory [subCategoryName=" + subCategoryName + ", category=" + category + ", productSet="
-				+ productSet + "]";
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((subCategoryName == null) ? 0 : subCategoryName.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		SubCategory other = (SubCategory) obj;
-		if (subCategoryName == null) {
-			if (other.subCategoryName != null)
-				return false;
-		} else if (!subCategoryName.equals(other.subCategoryName))
-			return false;
-		return true;
-	}
+		return "SubCategory [subCategoryName=" + subCategoryName
+				+ ", category=" + category + ", productSet=" + productSet + "]";
+	}*/
 
 }
